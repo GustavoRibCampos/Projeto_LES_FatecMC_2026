a73571a3ac3b4cd47f01bc4dd7ca4858b3d8f12f
GustavoRibCampos <gustavoribeirodecampos5@gmail.com>
2026-03-18 11:58:26 -0300

    Backup

--- Controller/ClienteController.js
+++ Controller/ClienteController.js
@@ -1,10 +1,34 @@
 const Cliente = require('../Model/Cliente')
 const ClienteDAO = require ('../DAO/ClienteDAO')
 const router = require('express').Router()
+const clientedao = new ClienteDAO()
 
 //Conexão para CRUD Cliente
-router.get('/clientes', (req, res) => {
-    res.send('ok')
+router.get('/clientes', async (req, res) => {
+    const resultado = await clientedao.lerCliente()
+    res.json(resultado)
 })
+
+router.get('/clientes/:id', async (req, res) => {
+    const resultado = await req.params.id
+    res.json(resultado)
+})
+
+router.post('/clientes/:id', async (req,res) => {
+    const resultado = await clientedao.criarCliente()
+    res.json(resultado)
+})
+
+router.put('/clientes/:id', async (req,res) => {
+    const resultado = await clientedao.atualizarCliente()
+    res.json(resultado)
+})
+
+router.delete('/clientes/:id', async (req, res) => {
+    const resultado = await clientedao.deletarCliente()
+    res.json(resultado)
+})
+
+router.post()
 
 module.exports = router
--- server.js
+++ server.js
@@ -14,3 +14,5 @@ app.listen(port, () =>
     console.log('Servidor rodando em http://localhost:`${port}`')
 })
 
+
+
